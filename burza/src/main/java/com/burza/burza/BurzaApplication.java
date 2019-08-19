70342aaf3be2d2101b10eed5995803ca6b0895d4
gurungarun <gurungarun995@gmail.com>
2019-08-19 09:53:15 -0700

    Initial commit

---
+++ burza/src/main/java/com/burza/burza/BurzaApplication.java
@@ -0,0 +1,13 @@
+package com.burza.burza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurzaApplication.class, args);
+	}
+
+}
---
+++ burza/src/main/java/com/burza/burza/rest/restController.java
@@ -0,0 +1,5 @@
+package com.burza.burza.rest;
+
+public class restController {
+
+}
